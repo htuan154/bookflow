a5f544330e501b122ddd5ed456fe3827b99afb00
kieulieu <kieulieu1001@gmail.com>
2025-07-24 13:15:18 +0700

    fixbug:router Admin

--- web_app/src/routes/AdminRoutes.js
+++ web_app/src/routes/AdminRoutes.js
@@ -1,10 +1,12 @@
 // src/routes/AdminRoutes.js
 import React from 'react';
-import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
+import AdminLayout from '../components/layout/AdminLayout'; // Import AdminLayout
 import AdminDashboardPage from '../pages/admin/AdminDashboardPage';
 import AdminWelcomePage from '../pages/admin/AdminWelcomePage';
 import { USER_ROLES } from '../constants/roles';
+
 const AdminRoutes = () => {
     const { isAuthenticated, user } = useAuth();
 
@@ -16,13 +18,13 @@ const AdminRoutes = () => {
         return <Navigate to="/unauthorized" replace />;
     }
 
-
-
     return (
-        <Routes>
-            <Route path="/" element={<AdminWelcomePage />} />
-            <Route path="/dashboard" element={<AdminDashboardPage />} />
-        </Routes>
+        <AdminLayout>
+            <Routes>
+                <Route path="/" element={<AdminWelcomePage />} />
+                <Route path="/dashboard" element={<AdminDashboardPage />} />
+            </Routes>
+        </AdminLayout>
     );
 };
 
